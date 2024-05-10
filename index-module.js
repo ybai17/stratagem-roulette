41bf5edbe5a5532a9befcf80332800b9c5b1a0e8
Yilei Bai <marbink100@gmail.com>
2024-05-10 17:17:46 -0400

    updated visuals

--- index-module.js
+++ index-module.js
@@ -1,31 +1,80 @@
 import * as roulette from './roulette-logic.js';
 import { SVG_All } from './strategem-svg-maps.js';
 
+let total_player_number = 4;
+let table_cell_width = 100;
+
 //default doesn't work
-export function load() {
+export function load_strats() {
     console.log("index-module.js LOAD");
 
     document.getElementById("players").replaceChildren();
 
     let players_div = document.getElementById("players");
 
-        for (let i = 0; i < 4; i++) {
-            let player_node = document.createElement("p");
-            player_node.id = "player" + i;
+        for (let i = 0; i < total_player_number; i++) {
+            let strategem_list = roulette.pickStrategemSet();
 
-            let strategem_set = roulette.pickStrategemSet();
-
-            player_node.innerHTML = strategem_set;
-            
-            players_div.appendChild(player_node);
-
-            strategem_set.forEach(strat => {
-                let strategem_svg = document.createElement("img");
-                strategem_svg.width = "100";
-                strategem_svg.height = "100";
-
-                strategem_svg.src = SVG_All[strat];
-                players_div.appendChild(strategem_svg);
-            });
+            create_player_strat_table(players_div, i, strategem_list);
         }
 }
+
+function create_player_strat_table(players_div, player_id, strategem_list) {
+
+    let player_table = document.createElement("table");
+    player_table.id = "player" + player_id;
+    player_table.style = "border: 3px solid; padding: 10px; margin: auto; table-layout: fixed; width: 800px;";
+
+    //player_table.innerHTML = strategem_list;
+    
+    let player_table_header_row = build_table_header(player_id, strategem_list);
+    let player_table_icon_row = build_table_icons(player_id, strategem_list);
+
+    player_table.appendChild(player_table_header_row);
+    player_table.appendChild(player_table_icon_row);
+    
+    players_div.appendChild(player_table);
+
+    players_div.appendChild(document.createElement("br"));
+}
+
+function build_table_header(player_id, strategem_list) {
+    let output = document.createElement("tr");
+    output.setAttribute("id", "player_" + player_id + "_header_row");
+    output.style = "text-align: center; border: 1 px solid;";
+
+    for (let i = 0; i < 4; i++) {
+        let player_table_headers_single = document.createElement("th");
+        player_table_headers_single.style = "padding: 10px; width: 100px";
+
+        player_table_headers_single.innerHTML = strategem_list[i];
+
+       output.appendChild(player_table_headers_single);
+    }
+
+    return output;
+}
+
+function build_table_icons(player_id, strategem_list) {
+    let output = document.createElement("tr");
+    output.setAttribute("id", "player_" + player_id + "_header_row");
+
+    strategem_list.forEach(strat => {
+        let icon_td = document.createElement("td");
+        icon_td.style = "text-align: center;";
+
+        let strategem_svg = document.createElement("img");
+        strategem_svg.width = "100";
+        strategem_svg.height = "100";
+
+        strategem_svg.src = SVG_All[strat];
+
+        icon_td.appendChild(strategem_svg);
+
+        output.appendChild(icon_td);
+    });
+
+    return output;
+}
+
+document.getElementById("reroll").addEventListener("click", load_strats);
--- index.js
+++ index.js
@@ -6,8 +6,6 @@ export function index_load() {
     console.log("_____________________index.js is LOADING");
 
     //console.log("function: " + index_module.load);
-
-    document.getElementById("reroll").addEventListener("click", index_module.load);
 }
 
 console.log("#### CLOSE function call inside index.js");
