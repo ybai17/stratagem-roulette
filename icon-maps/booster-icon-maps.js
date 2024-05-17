ada687c4eaa5eb111a7cdf53066f256b2ffc4cf0
Yilei Bai <marbink100@gmail.com>
2024-05-17 16:10:58 -0400

    adjusted spacing of file paths for easier reading

--- icon-maps/booster-icon-maps.js
+++ icon-maps/booster-icon-maps.js
@@ -2,14 +2,14 @@
 //file names are case sensitive in GitHub pages
 
 export const Booster_Icons = {
-    "Hellpod Space Optimization" : "./icons/Boosters/Hellpod Space Optimization.webp",
-    "Vitality Enhancement" : "./icons/Boosters/Vitality Enhancement.webp",
-    "UAV Recon"  : "./icons/Boosters/UAV Recon.webp",
-    "Stamina Enhancement" : "./icons/Boosters/Stamina Enhancement.webp",
-    "Muscle Enhancement" : "./icons/Boosters/Muscle Enhancement.webp",
+    "Hellpod Space Optimization"     : "./icons/Boosters/Hellpod Space Optimization.webp",
+    "Vitality Enhancement"           : "./icons/Boosters/Vitality Enhancement.webp",
+    "UAV Recon"                      : "./icons/Boosters/UAV Recon.webp",
+    "Stamina Enhancement"            : "./icons/Boosters/Stamina Enhancement.webp",
+    "Muscle Enhancement"             : "./icons/Boosters/Muscle Enhancement.webp",
     "Increased Reinforcement Budget" : "./icons/Boosters/Increased Reinforcement Budget.webp",
-    "Flexible Reinforcement Budget" : "./icons/Boosters/Hellpod Space Optimization.webp",
-    "Localization Confusion" : "./icons/Boosters/Localization Confusion.webp",
-    "Expert Extraction Pilot" : "./icons/Boosters/Expert Extraction Pilot.webp",
-    "Motivational Shocks": "./icons/Boosters/Motivational Shocks.png",
+    "Flexible Reinforcement Budget"  : "./icons/Boosters/Hellpod Space Optimization.webp",
+    "Localization Confusion"         : "./icons/Boosters/Localization Confusion.webp",
+    "Expert Extraction Pilot"        : "./icons/Boosters/Expert Extraction Pilot.webp",
+    "Motivational Shocks"            : "./icons/Boosters/Motivational Shocks.png",
 };
--- icon-maps/greande-icon-maps.js
+++ icon-maps/greande-icon-maps.js
@@ -1,10 +1,10 @@
 export const Grenade_Icons = {
-    "High Explosive": "./icons/Grenades/High Explosive.webp",
-    "Frag": "./icons/Grenades/Frag.webp",
-    "Impact": "./icons/Grenades/Impact.webp",
-    "Smoke": "./icons/Grenades/Smoke.webp",
-    "Incendiary": "./icons/Grenades/Incendiary.webp",
-    "Stun": "./icons/Grenades/Stun.webp",
-    "Thermite": "./icons/Grenades/Thermite.webp",
-    "Incendiary Impact": "./icons/Grenades/Incendiary Impact.webp",
+    "High Explosive"    : "./icons/Grenades/High Explosive.webp",
+    "Frag"              : "./icons/Grenades/Frag.webp",
+    "Impact"            : "./icons/Grenades/Impact.webp",
+    "Smoke"             : "./icons/Grenades/Smoke.webp",
+    "Incendiary"        : "./icons/Grenades/Incendiary.webp",
+    "Stun"              : "./icons/Grenades/Stun.webp",
+    "Thermite"          : "./icons/Grenades/Thermite.webp",
+    "Incendiary Impact" : "./icons/Grenades/Incendiary Impact.webp",
 };
